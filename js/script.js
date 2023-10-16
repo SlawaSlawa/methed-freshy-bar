0f21185ae0880a20c4d6c772a1ae7e340ecde3da
SlawaSlawa <slawapocht4@yandex.ru>
2023-10-17 00:45:59 +0400

    day: 4

--- js/script.js
+++ js/script.js
@@ -36,6 +36,28 @@ const createCard = (item) => {
     return cocktail
 }
 
+const scrollService = {
+    scrollPosition: 0,
+    disabledScroll() {
+        this.scrollPosition = window.scrollY
+        document.documentElement.style.scrollBehavior = 'auto'
+        document.body.style.cssText = `
+            overflow: hidden;
+            position: fixed;
+            top: -${this.scrollPosition}px;
+            left: 0;
+            height: 100vh;
+            width: 100vw;
+            padding-right: ${window.innerWidth - document.body.offsetWidth}px;
+        `
+    },
+    enabledScroll() {
+        document.body.style.cssText = ''
+        window.scroll({ top: this.scrollPosition })
+        document.documentElement.style.scrollBehavior = ''
+    }
+}
+
 const modalController = ({modal, btnOpen, time = 300}) => {
     const buttonElem = document.querySelector(btnOpen)
     const modalElem = document.querySelector(modal)
@@ -53,8 +75,10 @@ const modalController = ({modal, btnOpen, time = 300}) => {
 
         if (target === modalElem || code === 'Escape') {
             modalElem.style.opacity = 0
+
             setTimeout(() => {
                 modalElem.style.visibility = 'hidden'
+                scrollService.enabledScroll()
             }, time)
             window.removeEventListener('keydown', closeModal)
         }
@@ -63,6 +87,7 @@ const modalController = ({modal, btnOpen, time = 300}) => {
     const openModal = () => {
         modalElem.style.visibility = 'visible'
         modalElem.style.opacity = 1
+        scrollService.disabledScroll()
         window.addEventListener('keydown', closeModal)
     }
 
@@ -76,6 +101,10 @@ const init = async () => {
     modalController({
         modal: '.modal_order',
         btnOpen: '.header__btn-order'
+    })
+    modalController({
+        modal: '.modal_make',
+        btnOpen: '.coctail__btn_make'
     })
 
     const goodsListEl = document.querySelector('.goods__list')
